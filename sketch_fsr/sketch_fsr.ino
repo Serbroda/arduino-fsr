ca17547a2a6e2f34747c4000ca6808761b29e01a
Danny Rottstegge <danny@rottstegge.net>
2018-09-29 14:59:29 +0200

    Improve

--- sketch_fsr/sketch_fsr.ino
+++ sketch_fsr/sketch_fsr.ino
@@ -1,42 +1,70 @@
+#include <Arduino.h>
 
-int fsrPin = 0;   // Port 0 definieren
-int fsrReading;   // Analoges Ergebnis aus Port 0
-int fsrVoltage;   // Umgerechnetes Ergebnis in Volt
- 
-void setup(void) {
-  // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
-}
- 
-void loop(void) {
-  fsrReading = readValue();  
- 
-  Serial.print("Analog reading = ");
-  Serial.println(fsrReading);     // the raw analog reading
+int fsrPin = 0;  // Pin A0 definieren
+int sensorValue; // Analoges Ergebnis aus Pin A0
+int voltage;     // Umgerechnetes Ergebnis in Volt
 
-  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
-  Serial.print("Voltage reading in mV = ");
-  Serial.print(fsrVoltage);  
- 
-  // We'll have a few threshholds, qualitatively determined
-  if (fsrReading < 10) {
-    Serial.println(" - No pressure");
-  } else if (fsrReading < 200) {
-    Serial.println(" - Light touch");
-  } else if (fsrReading < 500) {
-    Serial.println(" - Light squeeze");
-  } else if (fsrReading < 800) {
-    Serial.println(" - Medium squeeze");
-  } else {
-    Serial.println(" - Big squeeze");
-  }
-
-  delay(1000);
-} 
-
-int readValue() {
-  return analogRead(fsrPin);
+void setup(void)
+{
+    Serial.begin(9600);
 }
 
+// Berechnet Volt aus dem analogen Ergebnis
+long calculateVoltage(long value)
+{
+    long inputMinimum = 0;
+    long inputMaximum = 1023;
+    long outputMinimum = 0;
+    long outputMaximum = 5000;
+    return (value - inputMinimum) * (outputMaximum - outputMinimum) / (inputMaximum - inputMinimum) + outputMinimum;
+}
 
+void printPlotter()
+{
+    Serial.println(voltage);
+}
 
+// Werte auf dem seriellen Monitor ausgeben
+void printMonitor()
+{
+    Serial.print("Wert: Roh = ");
+    Serial.print(sensorValue);
+    Serial.print(", mV = ");
+    Serial.print(voltage);
+
+    if (sensorValue < 10)
+    {
+        Serial.println(" - Nicht berührt");
+    }
+    else if (sensorValue < 100)
+    {
+        Serial.println(" - Leicht berührt");
+    }
+    else if (sensorValue < 300)
+    {
+        Serial.println(" - Leicht gedrückt");
+    }
+    else if (sensorValue < 600)
+    {
+        Serial.println(" - Mittel gedrückt");
+    }
+    else
+    {
+        Serial.println(" - Hart gedrückt");
+    }
+}
+
+void loop(void)
+{
+    // 1. Sensor über definierten Pin auslesen
+    sensorValue = analogRead(fsrPin);
+
+    // 2. Wert in Volt umrechnen
+    voltage = calculateVoltage(sensorValue);
+
+    // 3. Wert ausgeben
+    printMonitor();
+    //printPlotter();
+
+    delay(10);
+}
