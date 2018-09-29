41e21751baf4fbf5bc1093a845ffd113be996b97
Danny Rottstegge <danny@rottstegge.net>
2018-09-29 17:32:19 +0200

    Create foto thing

--- sketch_fsr/sketch_fsr.ino
+++ sketch_fsr/sketch_fsr.ino
@@ -4,19 +4,25 @@ int fsrPin = 0;  // Pin A0 definieren
 int sensorValue; // Analoges Ergebnis aus Pin A0
 int voltage;     // Umgerechnetes Ergebnis in Volt
 
+const int size = 5;
+
+long voltageArray[size];
+int counter = 0;
+
+// Einstieg für Arduino, bevor loop()
 void setup(void)
 {
-    Serial.begin(9600);
+    Serial.begin(115200); // Baudrate setzen
 }
 
 // Berechnet Volt aus dem analogen Ergebnis
 long calculateVoltage(long value)
 {
-    long inputMinimum = 0;
-    long inputMaximum = 1023;
-    long outputMinimum = 0;
-    long outputMaximum = 5000;
-    return (value - inputMinimum) * (outputMaximum - outputMinimum) / (inputMaximum - inputMinimum) + outputMinimum;
+    long bitMin = 0;
+    long bitMax = 1023;
+    long voltMin = 0;
+    long voltMax = 5000;
+    return (value - bitMin) * (voltMax - voltMin) / (bitMax - bitMin) + voltMin;
 }
 
 void printPlotter()
@@ -24,15 +30,26 @@ void printPlotter()
     Serial.println(voltage);
 }
 
-// Werte auf dem seriellen Monitor ausgeben
-void printMonitor()
+void printFoto(float s)
 {
-    Serial.print("Wert: Roh = ");
-    Serial.print(sensorValue);
-    Serial.print(", mV = ");
-    Serial.print(voltage);
+    if (s > 1000)
+    {
+        Serial.println("Foto!!!!!");
+    }
+}
 
-    if (sensorValue < 10)
+// Werte auf dem seriellen Monitor ausgeben
+void printMonitor(float s)
+{
+
+    Serial.print("Wert: Sensor = ");
+    Serial.print(sensorValue);
+    Serial.print(",    mV = ");
+    Serial.print(voltage);
+    Serial.print(",    s = ");
+    Serial.print(s);
+
+    /*if (sensorValue < 10)
     {
         Serial.println(" - Nicht berührt");
     }
@@ -51,7 +68,66 @@ void printMonitor()
     else
     {
         Serial.println(" - Hart gedrückt");
+    }*/
+
+    if (s > 1000)
+    {
+        Serial.println("---------------- FOTO ------------------");
     }
+}
+
+void addValue(long value)
+{
+    if (counter < size)
+    {
+        voltageArray[counter] = value;
+        counter++;
+    }
+    else
+    {
+        /*for (int i = 0; i < size, i++)
+        {
+            // solange die größe des arrays nicht erreicht ist
+            // sind wir in diesem block
+        }*/
+
+        voltageArray[0] = voltageArray[1];
+        voltageArray[1] = voltageArray[2];
+        voltageArray[2] = voltageArray[3];
+        voltageArray[3] = voltageArray[4];
+        voltageArray[4] = value;
+    }
+}
+
+float calculateAverage()
+{
+    float avg = 0;
+    for (int i = 0; i < size; i++)
+    {
+        avg = avg + voltageArray[i];
+    }
+
+    /*avg = avg + voltageArray[0];
+    avg = avg + voltageArray[1];
+    avg = avg + voltageArray[2];
+    avg = avg + voltageArray[3];
+    avg = avg + voltageArray[4];*/
+    return avg / size;
+}
+
+float calculateArithmeticAverage()
+{
+    float average = calculateAverage();
+
+    float s = 0;
+    for (int i = 0; i < size; i++)
+    {
+        s = s + powf(average - voltageArray[i], 2);
+    }
+
+    s = s / size;
+    s = powf(s, 0.5);
+    return s;
 }
 
 void loop(void)
@@ -62,9 +138,15 @@ void loop(void)
     // 2. Wert in Volt umrechnen
     voltage = calculateVoltage(sensorValue);
 
+    float arithmeticAverage = calculateArithmeticAverage();
+
+    addValue(voltage);
+
     // 3. Wert ausgeben
-    printMonitor();
+    printMonitor(arithmeticAverage);
     //printPlotter();
+    //printFoto(arithmeticAverage);
 
     delay(10);
 }
+
